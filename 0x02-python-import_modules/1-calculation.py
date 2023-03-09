a06147b400b8a5943c73e9402107c54b688ebbea
agachine <58729213+agachine@users.noreply.github.com>
2023-03-09 20:52:13 +0300

    Create 1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+rom caluclator_1 import add
+from calculator_1 import sub
+from calculator_1 import mul
+from calculator_1 import div
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a=a, b=b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a=a, b=b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a=a, b=b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, div(a=a, b=b)))
