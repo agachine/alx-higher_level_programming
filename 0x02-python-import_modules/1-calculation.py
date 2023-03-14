380b4bc60c0ede52df7fcbe244e53844ad53acd9
agachine <58729213+agachine@users.noreply.github.com>
2023-03-14 11:42:53 +0300

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import calculator_1
     a = 10
     b = 5
-    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a=a, b=b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a=a, b=b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a=a, b=b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, div(a=a, b=b)))
