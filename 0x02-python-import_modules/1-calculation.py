d634753273583d12a90b1aba6ba698377a358201
agachine <58729213+agachine@users.noreply.github.com>
2023-03-14 11:40:29 +0300

    imports functions, does math and prints result

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-from caluclator_1 import add
-from calculator_1 import sub
-from calculator_1 import mul
-from calculator_1 import div
 if __name__ == "__main__":
+    import calculator_1
     a = 10
     b = 5
-    print('{:d} + {:d} = {:d}'.format(a, b, add(a=a, b=b)))
-    print('{:d} - {:d} = {:d}'.format(a, b, sub(a=a, b=b)))
-    print('{:d} * {:d} = {:d}'.format(a, b, mul(a=a, b=b)))
-    print('{:d} / {:d} = {:d}'.format(a, b, div(a=a, b=b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
